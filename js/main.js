cf02b0ab67352919401eabb94b9748faac0f267b
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-03-29 10:25:07 +0200

    add three functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const button = document.querySelector('.js-button');
+const selectNumber = document.querySelector('.js-select');
+const winLose = document.querySelector('.js-title');
+const money = document.querySelector('.js-money');
+const inputBet = document.querySelector('.js-input');
+
+
+
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+
+function generatorComparatorNum() {
+    const getNumber = getRandomNumber(6);
+    const selectNum = selectNumber;
+
+if (getNumber === selectNum) {
+winLose.innerHTML = `Has ganado el doble de lo apostado :)`;
+
+} else { winLose.innerHTML = `Has perdido lo apostado :(`;
+
+}
+}
+
+
+function moneyBet() {
+    const chosenMoney = inputBet.value;
+    
+}
+
+function handleClicKButton (event) {
+    event.preventDefault();
+
+    getRandomNumber(6);
+    generatorComparatorNum ();
+    moneyBet();
+    
+}
+
+
+button.addEventListener('click' , handleClicKButton );
