4315484a79b9b42c7c2dc20fbaf6b95bbd6209f2
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-04-04 21:44:42 +0200

    Add some  solved basic issues

--- js/main.js
+++ js/main.js
@@ -6,44 +6,33 @@ const winLose = document.querySelector('.js-title');
 const money = document.querySelector('.js-money');
 const inputBet = document.querySelector('.js-input');
 
-
-
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-    }
-
-function generatorComparatorNum() {
-    const getNumber = getRandomNumber(6);
-    const selectNum = selectNumber;
-
-if (getNumber === parseInt(selectNum.value)) {
-
-winLose.innerHTML = `Has ganado el doble de lo apostado :)`;
-money.innetHTML = inputBet.value * 2;
-
-} else { winLose.innerHTML = `Has perdido lo apostado :(`;
-money.innerHTML = 0;
-
+  return Math.ceil(Math.random() * max);
 }
-}
+getRandomNumber(6);
 
+function messageBet() {
+  const getNumber = getRandomNumber(6);
+  const selectNum = selectNumber;
+
+  if (getNumber === parseInt(selectNum.value)) {
+    winLose.innerHTML = `Has ganado el doble de lo apostado :)`;
+    money.innetHTML = inputBet.value * 2;
+  } else {
+    winLose.innerHTML = `Has perdido lo apostado :(`;
+    money.innerHTML = 0;
+  }
+}
 
 function moneyBet() {
-    const chosenMoney = inputBet.value;
-    money.innerHTML = chosenMoney;
-
-    
+  const chosenMoney = inputBet.value;
+  money.innerHTML = chosenMoney;
 }
 
-function handleClicKButton (event) {
-    event.preventDefault();
-
-    getRandomNumber(6);
-    generatorComparatorNum ();
-    
+function handleClicKButton(event) {
+  event.preventDefault();
+  messageBet();
 }
 
-
-button.addEventListener('click' , handleClicKButton );
+button.addEventListener('click', handleClicKButton);
 inputBet.addEventListener('keyup', moneyBet);
-
